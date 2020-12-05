97a0ef16e5a415ce84be5908da4fa7f1afbcabd8
tnakatani <4361007+tnakatani@users.noreply.github.com>
2020-12-04 21:33:24 -0500

    commit list & tuples section

--- src/lists_tuples/list_tuples.py
+++ src/lists_tuples/list_tuples.py
@@ -1,4 +1,7 @@
-from typing import List
+from typing import List, Set, Tuple, Dict
+import operator
+from operator import itemgetter
+from collections import Counter, namedtuple
 
 
 def firstlast(seq):
@@ -53,4 +56,82 @@ def sum_numeric(*args):
         except ValueError:
             pass
     return result
+
+
+def alphabetize_names(list_of_dicts: List[Dict]) -> List[Dict]:
+    """Returns the list of dicts, but sorted by last name and then by first
+    name."""
+    return sorted(list_of_dicts,
+                  key=itemgetter('last', 'first'))
+
+    # alternate solution
+    # return sorted(names, key=lambda names: (names['last'], names['first']))
+
+
+def count_vowels(list_of_strings: List[str]) -> List[str]:
+    """Given a list of strings, sorts them according to how many vowels they
+    contain."""
+    vowels = 'aeiou'
+    vowel_counts = []
+    for word in list_of_strings:
+        vowel_count = {}
+        count = 0
+        for letter in word:
+            count += 1 if letter in vowels else 0
+        vowel_count['word'] = word
+        vowel_count['count'] = count
+        vowel_counts.append(vowel_count)
+    sorted_dict = sorted(vowel_counts, key=itemgetter('count'))
+    sorted_list = [x['word'] for x in sorted_dict]
+    return sorted_list
+
+
+def count_vowels_v2(list_of_strings: List[str]) -> List[str]:
+    """Given a list of strings, sorts them according to how many vowels they
+    contain."""
+    vowels = 'aeiou'
+    return sorted(list_of_strings, key=lambda word: sum(ch in vowels for ch in
+                                                        word))
+
+
+def sort_summed(list_of_ints: List[List[int]]) -> List[int]:
+    """Given a list of lists, with each list containing zero or more numbers,
+    sort by the sum of each inner list’s numbers."""
+    sorted_nums = sorted(list_of_ints, key=lambda nums: sum(nums))
+    return [sum(nums) for nums in sorted_nums]
+
+
+def most_repeating_letter_count(word: str) -> int:
+    """Given a non-empty string, counts how
+    many times each letter appears in the string,
+    and returns an integer indicating how often
+    the most common letter appears."""
+    return Counter(word).most_common(1)[0][1]
+
+
+def most_repeating_word(words: List[str]) -> str:
+    """Takes a sequence of strings as input and returns the
+    string that contains the greatest number of repeated letters."""
+    return max(words, key=most_repeating_letter_count)
+
+
+def format_sort_records(people: Tuple[str]) -> str:
+    """Takes a list of tuples and returns a sorted, tab-separated string"""
+    # {1:10} tells Python to display the item with index 1, inserting spaces
+    # if the data contains fewer than 10 characters
+    template = "{1:10} {0:10} {2:5.2f}"
+    output = []
+    for person in sorted(people, key=itemgetter(1, 0)):
+        print(person)
+        output.append(template.format(*person))
+    return output
+
+
+def format_sort_records_v2(list_of_tuples):
+    output = []
+    template = '{last:10} {first:10} {distance:5.2f}'
+    for person in sorted(list_of_tuples, key=operator.attrgetter('last', 'first')):
+        # https://docs.python.org/3/tutorial/controlflow.html#unpacking-argument-lists
+        output.append(template.format(**(person._asdict())))
+    return output
 
--- test/lists_tuples/test_list_tuples.py
+++ test/lists_tuples/test_list_tuples.py
@@ -1,5 +1,6 @@
 import pytest
 from src.lists_tuples.list_tuples import *
+from collections import namedtuple
 
 
 @pytest.mark.parametrize('inputs, expected', [
@@ -41,3 +42,77 @@ def test_mysum(inputs, expected):
 ])
 def test_sum_numeric(inputs, expected):
     assert sum_numeric(*inputs) == expected
+
+
+PEOPLE_1 = [{'first': 'Reuven', 'last': 'Lerner',
+           'email': 'reuven@lerner.co.il'},
+          {'first': 'Donald', 'last': 'Trump',
+           'email': 'president@whitehouse.gov'},
+          {'first': 'Vladimir', 'last': 'Putin',
+           'email': 'president@kremvax.ru'}
+          ]
+
+
+def test_alphabetize_names():
+    assert PEOPLE_1[0]['last'] == 'Lerner'
+    assert PEOPLE_1[1]['last'] == 'Trump'
+    assert PEOPLE_1[2]['last'] == 'Putin'
+
+    output = alphabetize_names(PEOPLE_1)
+    assert output[0]['last'] == 'Lerner'
+    assert output[1]['last'] == 'Putin'
+    assert output[2]['last'] == 'Trump'
+
+
+@pytest.mark.parametrize('inputs, expected', [
+    (['foo', 'bzz', 'bar', 'booboo'], ['bzz', 'bar', 'foo', 'booboo'])
+])
+def test_count_vowels(inputs, expected):
+    assert count_vowels(inputs) == expected
+    assert count_vowels_v2(inputs) == expected
+
+
+@pytest.mark.parametrize('inputs, expected', [
+    ([[1, 2], [0, 1], [5, 4]], [1, 3, 9])
+])
+def test_sort_summed(inputs, expected):
+    assert sort_summed(inputs) == expected
+
+
+@pytest.mark.parametrize('inputs, expected', [
+    (['this', 'is', 'an', 'elementary', 'test', 'example'], 'elementary')
+])
+def test_most_repeating_word(inputs, expected):
+    assert most_repeating_word(inputs) == expected
+
+
+PEOPLE_2 = [('Donald', 'Trump', 7.85),
+          ('Vladimir', 'Putin', 3.626),
+          ('Jinping', 'Xi', 10.603)]
+
+
+def test_with_people():
+    output = format_sort_records(PEOPLE_2)
+    assert isinstance(output, list)
+    assert all(isinstance(x, str) for x in output)
+
+    assert output[0][:10].strip() == 'Putin'
+    assert output[0][10:20].strip() == 'Vladimir'
+    assert output[0][20:].strip() == '3.63'
+
+
+Person = namedtuple('Person', ['first', 'last', 'distance'])
+
+
+PEOPLE_3 = [Person('Donald', 'Trump', 7.85),
+          Person('Vladimir', 'Putin', 3.626),
+          Person('Jinping', 'Xi', 10.603)]
+
+def test_with_people_v2():
+    output = format_sort_records_v2(PEOPLE_3)
+    assert isinstance(output, list)
+    assert all(isinstance(x, str) for x in output)
+
+    assert output[0][:10].strip() == 'Putin'
+    assert output[0][10:20].strip() == 'Vladimir'
+    assert output[0][20:].strip() == '3.63'
