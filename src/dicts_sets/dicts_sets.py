62ea30eb1ad60f07ded22cac388475163c121e4f
tnakatani <4361007+tnakatani@users.noreply.github.com>
2020-12-06 21:27:18 -0500

    commit date temperature, rainfall and word length exercises and tests

--- src/dicts_sets/dicts_sets.py
+++ src/dicts_sets/dicts_sets.py
@@ -1,9 +1,25 @@
-from collections import Counter, defaultdict
+from collections import Counter, defaultdict, OrderedDict
+from datetime import datetime, timedelta
+import os
+from typing import Dict, Set, List
+from functools import reduce
 
 MENU = {'sandwich': 9.99,
         'fries': 2.99,
         'coffee': 1.99,
         'pancakes': 6.99}
+
+TEMPS = {
+    '11/29/2020': 40,
+    '11/30/2020': 49,
+    '12/01/2020': 40,
+    '12/02/2020': 52,
+    '12/03/2020': 55,
+}
+
+
+######################################################################
+# Menu Items Exercise
 
 
 def normalize(text: str) -> str:
@@ -11,7 +27,14 @@ def normalize(text: str) -> str:
     return text.lower().strip()
 
 
-def restaurant(menu):
+def restaurant(menu: Dict[str, float]) -> None:
+    """Asks the user to enter an order:
+    - If the user enters the name of a dish on the menu, the program prints the
+    price and the running total. It then asks the user again for their order.
+    - If the user enters the name of a dish not on the menu, the program
+     scolds the user (mildly). It then asks the user again for their order.
+    - If the user enters 'done', prints the total price.
+    """
     total = 0
     while True:
         item = normalize(input('Order (type "done" to complete order): '))
@@ -28,3 +51,129 @@ def restaurant(menu):
             else:
                 print(f"Sorry, we are fresh out of {item}")
 
+
+######################################################################
+# Date Temperature Exercise
+
+
+def get_yesterday_and_tomorrow_date(day: str) -> str:
+    """Takes a date string in MM/DD/YYYY format and returns the date 1 day
+    before/after in the same format"""
+    _date = datetime.strptime(day, '%m/%d/%Y')
+    yesterday = (_date.date() - timedelta(days=1)).strftime("%m/%d/%Y")
+    tomorrow = (_date.date() + timedelta(days=1)).strftime("%m/%d/%Y")
+    return yesterday, tomorrow
+
+
+def get_temp_by_date(temps: Dict[str, int]) -> None:
+    """Asks the user to enter a date, and display the temperature on that
+    date, as well as the previous and subsequent dates, if available."""
+    while True:
+        date = normalize(input('Select date (type "done" to complete): '))
+        if not date:
+            print("No query registered, try again.")
+        if date == "done":
+            break
+        else:
+            if date not in temps:
+                print(f"Date not found, try again.")
+                continue
+            yesterday, tomorrow = get_yesterday_and_tomorrow_date(date)
+            try:
+                yesterday_temp = temps[yesterday]
+                tomorrow_temp = temps[tomorrow]
+            except KeyError:
+                print("Date range is out of bounds, try again.")
+                continue
+            temp = temps[date]
+            print(f"{yesterday} temperature: {yesterday_temp}")
+            print(f"{date} temperature: {temp}")
+            print(f"{tomorrow} temperature: {tomorrow_temp}")
+
+
+######################################################################
+# Rainfall Exercise
+
+
+def dict_is_empty(_dict: Dict) -> bool:
+    return bool(_dict) == False
+
+
+def rainfall_report() -> None:
+    """Tracks rainfall in a number of cities.
+    - Users is prompted to enter the name of a city;
+    - If the city name is blank, then the function prints a report before
+    exiting.
+    - If the city name isn’t blank, then the program asks the
+    user how much rain has fallen in that city (in millimeters).
+    - After the user enters the quantity of rain, the program again asks
+    them for a city name, rainfall amount, and so on--until the user presses
+    Enter instead of typing the name of a city.
+
+    Example output:
+    Boston: 10
+    New York: 7
+    """
+    report = {}
+    while True:
+        city = normalize(input("Enter the name of a city: ")).capitalize()
+        if not city:
+            break
+        rainfall = normalize(input(f"Enter the reported rainfall in"
+                                   f" {city} (in millimeters): "))
+        if rainfall.isdigit():
+            rainfall = int(rainfall)
+        else:
+            print('Not a valid value, try again.')
+            continue
+        # with two arguments, dict.get returns either the value associated
+        # with the key or the second argument
+        report[city] = report.get(city, 0) + rainfall
+    if dict_is_empty(report):
+        print("No data registered.")
+    else:
+        print("Reported rainfall:")
+        for k, v in report.items():
+            print(f"{k}: {v}mm")
+
+
+######################################################################
+# Word Length Counter
+
+def tokenize(text: List[str]) -> List[str]:
+    """Takes in a list of newline delimited text and returns a list of string
+    tokens"""
+    # Remove newlines
+    text = [l.rstrip() for l in text]
+    # Remove full-width whitespace
+    text = [l.replace('\u3000', ' ') for l in text]
+    # Split by white space
+    text = [l.split(' ') for l in text]
+    # Flatten list of lists
+    tokenized = reduce(lambda x, y: x + y, text)
+    # Above is equivalent to:
+    # for line in text:
+    #     for token in line:
+    #         tokens.append(token)
+    # Or:
+    # tokens = [token for line in text for token in line]
+    return tokenized
+
+
+def count_characters(token: str) -> int:
+    """Counts how many characters there in a token"""
+    len(token)
+
+
+def word_length_freq_table(file_path: str) -> Dict[int, int]:
+    """Read through a text file on disk. Use a dict to track how many words
+    of each length are in the file--that is, how many three-letter words,
+    four-letter words, five-letter words, and so on."""
+    with open(file_path, 'r', encoding='utf8') as f:
+        lines = f.readlines()
+        tokenized = tokenize(lines)
+        word_length = {}
+        for token in tokenized:
+            chars = len(token)
+            word_length[chars] = word_length.get(chars, 0) + 1
+        return word_length
--- test/dicts_sets/test_dicts_sets.py
+++ test/dicts_sets/test_dicts_sets.py
@@ -1,9 +1,12 @@
 import pytest
-
+import mock
 from src.dicts_sets.dicts_sets import *
 from collections import Counter, defaultdict
 from io import StringIO
 
+
+######################################################################
+# Menu Items Exercise
 
 def test_order_nothing(monkeypatch, capsys):
     monkeypatch.setattr('sys.stdin', StringIO("done"))
@@ -54,3 +57,118 @@ def test_order_item_not_on_menu(monkeypatch, capsys):
     captured_out, captured_err = capsys.readouterr()
     assert f"Sorry, we are fresh out of steak" in captured_out
     assert captured_out.endswith(f"Your total is 0\n")
+
+
+######################################################################
+# Date Temperature Exercise
+
+def test_no_date(monkeypatch, capsys):
+    monkeypatch.setattr('sys.stdin', StringIO("\n" +
+                                              "done\n"))
+    get_temp_by_date(TEMPS)
+    captured_out, captured_err = capsys.readouterr()
+    assert f"No query registered, try again" in captured_out
+
+
+def test_one_date(monkeypatch, capsys):
+    monkeypatch.setattr('sys.stdin', StringIO("11/30/2020" + "\n" +
+                                              "done\n"))
+    get_temp_by_date(TEMPS)
+    captured_out, captured_err = capsys.readouterr()
+    assert f"11/29/2020 temperature: 40" in captured_out
+    assert f"11/30/2020 temperature: 49" in captured_out
+    assert f"12/01/2020 temperature: 40" in captured_out
+
+
+def test_out_of_date(monkeypatch, capsys):
+    monkeypatch.setattr('sys.stdin', StringIO("11/29/2020" + "\n" +
+                                              "done\n"))
+    get_temp_by_date(TEMPS)
+    captured_out, captured_err = capsys.readouterr()
+    assert f"Date range is out of bounds, try again." in captured_out
+
+
+######################################################################
+# Rainfall Exercise
+
+
+def test_no_city(monkeypatch, capsys):
+    monkeypatch.setattr("sys.stdin", StringIO("\n\n"))
+    rainfall_report()
+    captured_out, captured_err = capsys.readouterr()
+    assert "No data registered." in captured_out
+
+
+def test_missing_rainfall(monkeypatch, capsys):
+    monkeypatch.setattr("sys.stdin", StringIO("Boston\n" +
+                                              "\n\n"))
+    rainfall_report()
+    captured_out, captured_err = capsys.readouterr()
+    assert "Not a valid value, try again." in captured_out
+
+
+def test_invalid_rainfall(monkeypatch, capsys):
+    monkeypatch.setattr("sys.stdin", StringIO("Boston\n" +
+                                              "\n" +
+                                              "foobar\n" +
+                                              "\n\n"))
+    rainfall_report()
+    captured_out, captured_err = capsys.readouterr()
+    assert "Not a valid value, try again." in captured_out
+
+
+def test_one_city(monkeypatch, capsys):
+    monkeypatch.setattr("sys.stdin", StringIO("Boston\n" +
+                                              "12\n" +
+                                              "\n\n"))
+    rainfall_report()
+    captured_out, captured_err = capsys.readouterr()
+    assert f"Boston: 12mm" in captured_out
+
+
+def test_multiple_city(monkeypatch, capsys):
+    monkeypatch.setattr("sys.stdin", StringIO("Boston\n" +
+                                              "12\n" +
+                                              "Seattle\n" +
+                                              "15\n" +
+                                              "Louisville\n" +
+                                              "7" +
+                                              "\n\n"))
+    rainfall_report()
+    captured_out, captured_err = capsys.readouterr()
+    assert f"Boston: 12mm" in captured_out
+    assert f"Seattle: 15mm" in captured_out
+    assert f"Louisville: 7mm" in captured_out
+
+
+def test_update_city(monkeypatch, capsys):
+    monkeypatch.setattr("sys.stdin", StringIO("Boston\n" +
+                                              "12\n" +
+                                              "Boston\n" +
+                                              "20\n" +
+                                              "\n\n"))
+    rainfall_report()
+    captured_out, captured_err = capsys.readouterr()
+    assert f"Boston: 32mm" in captured_out
+
+
+######################################################################
+# Word Length Counter
+
+
+"""
+Note: Had trouble figuring out whether mock.patch needed "__builtins__.open"
+or "builtins.open" or "main.open".  Still don't quite understand the nuance
+between all of them.  One important note is that the 'file_path' passed to the
+function doesn't really matter, you can pass it "file_path", "foobar", etc.
+
+Ref (led me to answer): https://stackoverflow.com/a/33184416/12207563
+Python doc: https://docs.python.org/3.6/library/unittest.mock.html#patch
+"""
+
+
+def test_word_length_freq_table():
+    mock_open = mock.mock_open(read_data='a ab abc abcd')
+    with mock.patch("builtins.open", mock_open) as m:
+        result = word_length_freq_table('file_path')
+    assert result == {1: 1, 2: 1, 3: 1, 4: 1}
