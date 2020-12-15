049e68acb63e07a27b06e7d90dded6141455942b
tnakatani <4361007+tnakatani@users.noreply.github.com>
2020-12-14 21:39:14 -0500

    add files code and tests

--- src/files/files.py
+++ src/files/files.py
@@ -1,18 +1,23 @@
-import csv, json
-import os, glob
-from functools import reduce
-from typing import Dict, Set, List, Union, Callable, Tuple
+import csv
+import json
 import logging
+import os
+import pathlib
+import re
+from collections import defaultdict
+from functools import reduce
+from typing import Dict, List
 
 
 ################################################################################
 # Final Line
 
+
 def final_line(file_path: str) -> str:
     """Takes a filename as an argument and returns that file’s final line"""
     with open(file_path) as f:
-        last_line = ''
-        for line in f:
+        last_line = ""
+        for _ in f:
             last_line = f.readline()
         return last_line
 
@@ -31,10 +36,10 @@ def sum_multi_columns(file_path: str) -> int:
     """Reads a two-column TSV. Multiplies each first number by the second,
     and then sums the results from all the lines.  Ignores any line that
     doesn’t contain two or more numeric columns."""
-    with open(file_path, 'r') as f:
+    with open(file_path, "r") as f:
         nums = []
         for line in f:
-            split = line.rstrip().split('\t')
+            split = line.rstrip().split("\t")
             if len(split) >= 2:
                 line = [int(i) for i in split]
                 nums.append(reduce(lambda x, y: x * y, line))
@@ -43,6 +48,7 @@ def sum_multi_columns(file_path: str) -> int:
 
 ################################################################################
 # /etc/passwd to dict
+
 
 def passwd_to_dict(file_path: str) -> Dict[any, Dict[str, str]]:
     """Returns a dict based on /etc/passwd in which the dict’s keys are
@@ -56,17 +62,16 @@ def passwd_to_dict(file_path: str) -> Dict[any, Dict[str, str]]:
     The first field (index 0) is the username (e.g. nobody), and the third
     field (index 2) is the user’s unique ID number (e.g. -2.
     """
-    with open(file_path, 'r') as f:
+    with open(file_path, "r") as f:
         users = {}
         for line in f:
             try:
-                fields = line.strip().split(':')
-                user, id, home_dir, shell = fields[0], fields[2], \
-                                            fields[5], fields[6]
-            except IndexError as e:
+                fields = line.strip().split(":")
+                user, id, home_dir, shell = fields[0], fields[2], fields[5], fields[6]
+            except IndexError:
                 logging.warning(f"{fields} has unexpected format, skipping")
                 continue
-            users[user] = {'id': id, 'home_dir': home_dir, 'shell': shell}
+            users[user] = {"id": id, "home_dir": home_dir, "shell": shell}
         return users
 
 
@@ -79,7 +84,7 @@ def count_chars(word_list: List[str]) -> int:
     doesn't include white space delimiters or newlines.
 
     Args:
-        text (str): a string of tokens of an arbitrary length
+        word_list (str): a string of tokens of an arbitrary length
 
     Returns:
         int: Number of characters
@@ -100,7 +105,35 @@ def normalize(text):
     Returns:
         tokens (List[str]): a list of normalized word tokens
     """
-    return text.strip().lower().split(' ')
+
+    def _replace_urls(text):
+        url_regex = (
+            r"(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,"
+            r"}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|("
+            r"?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})"
+        )
+        text = re.sub(url_regex, "<URL>", text)
+        return text
+
+    def _simplify_punctuation(text):
+        """
+        This functions simplifies doubled or more complex punctuation. The exception is '...'.
+        """
+        corrected = str(text)
+        corrected = re.sub(r"([!?,;])\1+", r"\1", corrected)
+        corrected = re.sub(r"\.{2,}", r"...", corrected)
+        return corrected
+
+    text = text.strip().lower()
+    text = _replace_urls(text)
+    text = _simplify_punctuation(text)
+    return text.split(" ")
+
+    # for token in tokens:
+    #     if "http" in token:
+    #         continue
+    #     norm.append(token)
+    return no_url
 
 
 def word_count(file_path: str) -> Dict[str, int]:
@@ -113,18 +146,200 @@ def word_count(file_path: str) -> Dict[str, int]:
     Args:
         file_path (str): Path to text file
     """
-    with open(file_path, 'r') as f:
-        counts = {'char_count': 0, 'word_count': 0, 'line_count': 0,
-                  'words_uniq': 0, }
+    with open(file_path, "r") as f:
+        counts = {
+            "char_count": 0,
+            "word_count": 0,
+            "line_count": 0,
+            "words_uniq": 0,
+        }
         all_words = set()
         for line in f:
             token_list = normalize(line)
-            counts['char_count'] += count_chars(token_list)
-            counts['word_count'] += len(token_list)
-            counts['line_count'] += 1
+            counts["char_count"] += count_chars(token_list)
+            counts["word_count"] += len(token_list)
+            counts["line_count"] += 1
             all_words.update(token_list)
-        counts['words_uniq'] = len(all_words)
+        counts["words_uniq"] = len(all_words)
         return counts
 
 
+################################################################################
+# Longest word per file/files
 
+
+def find_longest_word(fp: str) -> str:
+    """Takes a filename as an argument and returns the longest word found in the file
+
+    Args:
+        fp: File path
+
+    Returns:
+        Longest word found in the file
+    """
+
+    # Attempt 1
+    # with open(fp, 'r') as f:
+    #     longest = ''
+    #     for line in f:
+    #         tokens = set(normalize(line))
+    #         words = {t:len(t) for t in tokens}
+    #         max_word = max(words, key=words.get)
+    #         if len(max_word) > len(longest):
+    #             longest = max_word
+    #     return longest
+
+    # Attempt 2
+    with open(fp, "r") as f:
+        words = set()
+        for line in f:
+            words.update(set(normalize(line)))
+        try:
+            return max(words, key=len)
+        except ValueError:
+            return ""
+
+
+def find_all_longest_words(fd: str) -> Dict[str, str]:
+    """Takes a directory name and returns a dict in which the keys are filenames and the values
+    are the longest words from each file.
+
+    Args:
+        fd: File directory
+
+    Returns:
+        Dict with filenames as keys, and longest words as value.
+    """
+
+    # Attempt 1:
+    # file_paths = os.listdir(fd)
+    # longest_words = {}
+    # for fp in file_paths:
+    #     longest_words[fp] = find_longest_word(os.path.join(fd, fp))
+
+    # Attempt 2:
+    file_paths = pathlib.Path(fd)
+    longest_words = {
+        fp.name: find_longest_word(fp)
+        for fp in file_paths.iterdir()
+        if file_paths.is_dir()
+    }
+    return longest_words
+
+
+################################################################################
+# Reading / Writing CSV
+
+
+def read_passwd(read_path: str) -> List[List[str]]:
+    """Read file and return a list of lists
+
+    Args:
+        read_path: Path to /etc/passwd file
+    """
+    # Attempt 1:
+    # rows = []
+    # with open(read_path, "r") as f:
+    #     for line in f:
+    #         try:
+    #             fields = line.strip().split(":")
+    #             user_and_id = [fields[0], fields[2]]
+    #         except IndexError:
+    #             logging.warning(f"{fields} has unexpected format, skipping")
+    #             continue
+    #         rows.append(user_and_id)
+    # return rows
+
+    # Attempt 2:
+    rows = []
+    with open(read_path, "r") as f:
+        reader = csv.reader(f, delimiter=":")
+        for line in reader:
+            if len(line) <= 1:
+                continue
+            rows.append([line[0], line[2]])
+    return rows
+
+
+def write_passwd(user_list: List[List[str]], write_path: str) -> None:
+    """Takes a list of lists and writes them to a TSV file
+
+    Args:
+        write_path: Path to output TSV file
+    """
+    with open(write_path, "w") as f:
+        writer = csv.writer(f, delimiter="\t")
+        for user in user_list:
+            writer.writerow(user)
+
+
+def passwd_to_tsv(read_path: str, write_path: str) -> None:
+    """Reads the standard Unix /etc/passwd file and writes a TSV file containing the username
+    and user ID. Ignores commented lines.
+
+    Args:
+        read_path: Path to /etc/passwd file
+        write_path: Path to output TSV file
+    """
+    # Attempt 1:
+    # rows = read_passwd(read_path)
+    # write_passwd(rows, write_path)
+
+    # Attempt 2:
+    with open(read_path, "r") as rf, open(write_path, "w") as wf:
+        reader = csv.reader(rf, delimiter=":")
+        writer = csv.writer(wf, delimiter="\t")
+        for line in reader:
+            if len(line) > 1:
+                writer.writerow([line[0], line[2]])
+
+
+################################################################################
+# JSON
+
+
+def analyze_scores(read_path: str, write_path: str) -> None:
+    """Reads a directory with multiple JSON files of test scores.  Calculates the highest, lowest,
+    and average test scores for each subject in each class, and writes them to a TSV file"""
+    file_paths = pathlib.Path(read_path).iterdir()
+    classes_results = {}
+    for path in file_paths:
+        with open(path, "r") as rf, open(write_path, "w") as wf:
+            # Read JSON
+            classes_results[path.stem] = defaultdict(list)
+            _scores = json.load(rf)
+            for _score in _scores:
+                for subject, subject_score in _score.items():
+                    # Setting defaultdict above is equivalent to below:
+                    # classes_results[path.stem].setdefault(subject, [])
+                    classes_results[path.stem][subject].append(subject_score)
+            # Write to TSV
+            writer = csv.writer(wf, delimiter="\t")
+            header = ["class", "subject", "min_score", "max_score", "avg_score"]
+            writer.writerow(header)
+            for _class in classes_results:
+                for _subject, _scores in classes_results[_class].items():
+                    _min, _max, _avg = (
+                        min(_scores),
+                        max(_scores),
+                        sum(_scores) / len(_scores),
+                    )
+                    writer.writerow([_class, _subject, _min, _max, _avg])
+
+
+################################################################################
+# Reverse Lines
+
+
+def reverse_lines(read_path: str, write_path: str) -> None:
+    """Reads a text file and writes to another path with the strings reversed.
+
+    Args:
+        read_path: Path to text file to read
+        write_path: Path to text file to write
+    """
+    with open(read_path, "r") as rf, open(write_path, "w") as wf:
+        for line in rf:
+            line = line.rstrip()
+            reverse = f"{line[::-1]}\n"
+            wf.write(reverse)
--- test/files/test_files.py
+++ test/files/test_files.py
@@ -1,6 +1,8 @@
-import pytest
-import mock
 from io import StringIO
+
+import mock
+import pytest
+
 from src.files.files import *
 
 
@@ -9,17 +11,17 @@ from src.files.files import *
 
 
 def test_empty():
-    mock_open = mock.mock_open(read_data='')
+    mock_open = mock.mock_open(read_data="")
     with mock.patch("builtins.open", mock_open) as m:
-        result = final_line('file_path')
-    assert result == ''
+        result = final_line("file_path")
+    assert result == ""
 
 
 def test_final_line():
-    mock_open = mock.mock_open(read_data='a\nab\nabc\nabcd')
+    mock_open = mock.mock_open(read_data="a\nab\nabc\nabcd")
     with mock.patch("builtins.open", mock_open) as m:
-        result = final_line('file_path')
-    assert result == 'abcd'
+        result = final_line("file_path")
+    assert result == "abcd"
 
 
 ################################################################################
@@ -27,49 +29,219 @@ def test_final_line():
 
 
 def test_multi_columns_multi_rows():
-    fake_tsv = StringIO('1\n'
-                        '1\t2\n'
-                        '1\t2\t3\n'
-                        '1\t2\t3\t4')
+    fake_tsv = StringIO("1\n" "1\t2\n" "1\t2\t3\n" "1\t2\t3\t4")
     with mock.patch("builtins.open", return_value=fake_tsv):
-        assert sum_multi_columns('file_path') == 32
+        assert sum_multi_columns("file_path") == 32
 
 
 ################################################################################
 # /etc/passwd to dict
 
+
 def test_passwd_to_dict():
     fake_passwd = StringIO(
-        '###############\n'
-        '# User Database\n'
-        '###############\n'
-        '               \n'
-        'nobody:*:-2:-2:Unprivileged User:/var/empty:/usr/bin/false\n'
-        'root:*:0:0:System Administrator:/var/root:/bin/sh\n'
-        'daemon:*:1:1:System Services:/var/root:/usr/bin/false\n'
-        'foobarbaz:incomplete_data:info\n'
+        "###############\n"
+        "# User Database\n"
+        "###############\n"
+        "               \n"
+        "nobody:*:-2:-2:Unprivileged User:/var/empty:/usr/bin/false\n"
+        "root:*:0:0:System Administrator:/var/root:/bin/sh\n"
+        "daemon:*:1:1:System Services:/var/root:/usr/bin/false\n"
+        "foobarbaz:incomplete_data:info\n"
     )
     with mock.patch("builtins.open", return_value=fake_passwd):
-        assert passwd_to_dict('file_path') == {
-            'daemon': {'home_dir': '/var/root', 'id': '1',
-                       'shell': '/usr/bin/false'},
-            'nobody': {'home_dir': '/var/empty', 'id': '-2',
-                       'shell': '/usr/bin/false'},
-            'root': {'home_dir': '/var/root', 'id': '0', 'shell': '/bin/sh'}}
+        assert passwd_to_dict("file_path") == {
+            "daemon": {"home_dir": "/var/root", "id": "1", "shell": "/usr/bin/false"},
+            "nobody": {"home_dir": "/var/empty", "id": "-2", "shell": "/usr/bin/false"},
+            "root": {"home_dir": "/var/root", "id": "0", "shell": "/bin/sh"},
+        }
 
 
 ################################################################################
 # Word Count
 
-def test_word_count():
-    text_file = StringIO(
-       "foo bar baz\n" # 11 chars
-       "foo bar 文書\n" # 10 chars
-    )
-    with mock.patch("builtins.open", return_value=text_file):
-        result = word_count('file_path')
-        assert result['line_count'] == 2
-        assert result['word_count'] == 6
-        assert result['words_uniq'] == 4
-        assert result['char_count'] == 17
 
+def test_word_count():
+    text_file = StringIO("foo bar baz\n" "foo bar 文書\n")  # 11 chars  # 10 chars
+    with mock.patch("builtins.open", return_value=text_file):
+        result = word_count("file_path")
+        assert result["line_count"] == 2
+        assert result["word_count"] == 6
+        assert result["words_uniq"] == 4
+        assert result["char_count"] == 17
+
+
+################################################################################
+# Longest word per file/files
+
+
+@pytest.fixture
+def empty_file(tmp_path):
+    f = tmp_path / "emptyfile.txt"
+    f.write_text("")
+    return f
+
+
+@pytest.fixture
+def small_file(tmp_path):
+    f = tmp_path / "smallfile.txt"
+    f.write_text(
+        """This is the first line
+and this is the second line"""
+    )
+    return f
+
+
+@pytest.fixture
+def big_file(tmp_path):
+    f = tmp_path / "bigfile.txt"
+    f.write_text(
+        """This is the first line of a big file
+and this is the second line
+and this is, to no one's surprise, the third line
+but the biggest word will probably be encyclopedia
+
+here is a website: https://www.gutenberg.org
+here are some garbled text: ??faa faa ;;;??
+"""
+    )
+    return f
+
+
+def test_small_file(small_file):
+    assert find_longest_word(small_file) == "second"
+
+
+def test_big_file(big_file):
+    assert find_longest_word(big_file) == "encyclopedia"
+
+
+def test_empty_directory(tmp_path):
+    assert find_all_longest_words(tmp_path) == {}
+
+
+def test_empty_file(tmp_path, empty_file):
+    assert find_all_longest_words(tmp_path) == {"emptyfile.txt": ""}
+
+
+def test_all_files(tmp_path, empty_file, small_file, big_file):
+    assert find_all_longest_words(tmp_path) == {
+        "emptyfile.txt": "",
+        "smallfile.txt": "second",
+        "bigfile.txt": "encyclopedia",
+    }
+
+
+################################################################################
+# Reading / Writing CSV
+
+
+@pytest.fixture
+def passwd_file(tmp_path):
+    f = tmp_path / "passwd"
+    f.write_text(
+        """
+root:x:0:0:root:/root:/bin/bash
+daemon:x:1:1:daemon:/usr/sbin:/usr/sbin/nologin
+bin:x:2:2:bin:/bin:/usr/sbin/nologin
+sys:x:3:3:sys:/dev:/usr/sbin/nologin
+sync:x:4:65534:sync:/bin:/bin/sync
+games:x:5:60:games:/usr/games:/usr/sbin/nologin
+man:x:6:12:man:/var/cache/man:/usr/sbin/nologin
+lp:x:7:7:lp:/var/spool/lpd:/usr/sbin/nologin
+mail:x:8:8:mail:/var/mail:/usr/sbin/nologin
+atara:x:1004:1005:Atara Lerner-Friedman,,,:/home/atara:/bin/bash
+shikma:x:1005:1006:Shikma Lerner-Friedman,,,:/home/shikma:/bin/bash
+# Here is a comment
+amotz:x:1006:1007:Amotz Lerner-Friedman,,,:/home/amotz:/bin/bash
+"""
+    )
+    return f
+
+
+def test_empty_file(tmp_path, empty_file):
+    passwd_to_tsv(empty_file, "output.tsv")
+    csv_content = open("output.tsv").read()
+    assert len(csv_content) == 0
+
+
+def test_passwd_to_csv(passwd_file):
+    passwd_to_tsv(passwd_file, "output.tsv")
+    csv_content = open("output.tsv").read()
+    assert len(csv_content) == 95
+    assert csv_content.splitlines()[0] == "root\t0"
+    assert csv_content.splitlines()[-1] == "amotz\t1006"
+
+
+################################################################################
+# JSON
+
+
+@pytest.fixture
+def score_file_1(tmp_path):
+    j1 = tmp_path / "9a.json"
+    j1.write_text(
+        """
+[{"math" : 90, "literature" : 98, "science" : 97},
+ {"math" : 65, "literature" : 79, "science" : 85},
+ {"math" : 78, "literature" : 83, "science" : 75},
+ {"math" : 92, "literature" : 78, "science" : 85},
+ {"math" : 100, "literature" : 80, "science" : 90}
+]
+"""
+    )
+    return j1
+
+
+@pytest.fixture
+def score_file_2(tmp_path):
+    j2 = tmp_path / "9b.json"
+    j2.write_text(
+        """
+[{"math" : 70, "literature" : 98, "science" : 97},
+ {"math" : 65, "literature" : 83, "science" : 70},
+ {"math" : 58, "literature" : 83, "science" : 75},
+ {"math" : 72, "literature" : 78, "science" : 85},
+ {"math" : 100, "literature" : 80, "science" : 90}
+]
+"""
+    )
+    return j2
+
+
+def test_score_file(tmp_path, score_file_1, score_file_2):
+    analyze_scores(tmp_path, "output.tsv")
+    csv_content = open("output.tsv").read()
+    assert csv_content.splitlines()[0].split("\t") == [
+        "class",
+        "subject",
+        "min_score",
+        "max_score",
+        "avg_score",
+    ]
+    assert csv_content.splitlines()[1].split("\t")[0] == "9a"
+    assert csv_content.splitlines()[6].split("\t")[0] == "9b"
+
+
+################################################################################
+# Reverse lines
+
+
+@pytest.fixture
+def big_file(tmp_path):
+    f = tmp_path / "bigfile.txt"
+    f.write_text(
+        """This is the first line of a big file
+and this is the second line
+and this is, to no one's surprise, the third line
+but the biggest word will probably be encyclopedia"""
+    )
+    return f
+
+
+def test_reversing_lines(big_file):
+    reverse_lines(big_file, "output.txt")
+    content = open("output.txt").read()
+    assert len(content) == 166
+    assert content[:18] == "elif gib a fo enil"
+    assert content[-18:] == "w tseggib eht tub\n"
