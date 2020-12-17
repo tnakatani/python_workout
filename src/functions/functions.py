52d8fb8d51ae029fd874c723771eb04c814284c2
tnakatani <4361007+tnakatani@users.noreply.github.com>
2020-12-16 19:23:25 -0500

    Add function module code and test

--- src/functions/functions.py
+++ src/functions/functions.py
@@ -1,3 +1,9 @@
+import operator
+import random
+import string
+from typing import Callable, List, Tuple
+
+
 ################################################################################
 # XML Generator
 
@@ -24,7 +30,9 @@ def myxml(tag: str, content: str = "", **kwargs) -> str:
 
     # Attempt 2:
     attributes = (
-        "".join([f" {key}={value}" for key, value in kwargs.items()]) if kwargs else ""
+        "".join([f' {key}="{value}"' for key, value in kwargs.items()])
+        if kwargs
+        else ""
     )
     return f"<{tag}{attributes}>{content}</{tag}>"
 
@@ -36,3 +44,97 @@ def factorial(*args) -> int:
     for arg in args:
         result = result * arg
     return result
+
+
+################################################################################
+# Prefix Notation Calculator
+
+
+def is_exp(iter: List[str]):
+    """Returns true if prefix notation operator is exponentiation (**)"""
+    return True if iter[:2] == "**" else False
+
+
+def calc(formula: str) -> int:
+    """Performs prefix notation calculation, ie. operator precedes the arguments.  Handles
+    addition, subtraction, multiplication, division (/), modulus (%), and exponentiation (**).
+
+    Args:
+        formula: a string containing a simple math expression in prefix notation--with an
+        operator and two numbers. Example: "+23" == 5
+    """
+    ops = {
+        "+": operator.add,
+        "-": operator.sub,
+        "*": operator.mul,
+        "/": operator.truediv,
+        "%": operator.mod,
+        "**": operator.pow,
+    }
+    # Expontential operator need special handling since it contains 2 chars
+    op, num_range = (
+        ("**", formula[2:]) if is_exp(formula) else (formula[0], formula[1:])
+    )
+    nums = [int(num) for num in num_range]
+    result = nums[0]
+    for num in nums[1:]:
+        result = ops[op](result, num)
+    return result
+
+
+################################################################################
+# Password Generator
+
+
+def create_password_generator(text: str) -> Callable:
+    """Returns a function that a creates a password of a specified length, using the string
+    passed as the argument.
+
+    Args:
+        text: strings to be used to create the password
+    """
+
+    def password_generator(length: int) -> str:
+        chars = []
+        for i in range(length):
+            chars.append(random.choice(text))
+        return "".join(chars)
+
+    return password_generator
+
+
+def create_password_checker(
+    min_uppercase: int, min_lowercase: int, min_punctuation: int, min_digits: int
+) -> Callable:
+    """Returns a function that checks that a given password meets the minimum occurrence criteria
+    for the arguments passed"""
+
+    def count_params(password: str) -> Tuple[int]:
+        """Counts the number of occurrences of each acceptance criteria"""
+        upper_case, lower_case, punctuation, digit = 0, 0, 0, 0
+        for char in password:
+            if char.isupper():
+                upper_case += 1
+            if char.islower():
+                lower_case += 1
+            if char in string.punctuation:
+                punctuation += 1
+            if char.isdigit():
+                digit += 1
+        return digit, lower_case, punctuation, upper_case
+
+    def count_meets_minimum(criteria_counts: Callable) -> bool:
+        """Checks whether the counts for each acceptance criteria meets the minimum threshold"""
+        for counts, minimum in zip(
+            criteria_counts,
+            (min_uppercase, min_lowercase, min_punctuation, min_digits),
+        ):
+            if counts >= minimum:
+                continue
+            return False
+        return True
+
+    def password_checker(password: str):
+        return count_meets_minimum(count_params(password))
+
+    return password_checker
--- test/functions/functions.py
+++ test/functions/functions.py
@@ -1,3 +1,5 @@
+import string
+import random
 import pytest
 
 from src.functions.functions import *
@@ -16,7 +18,22 @@ def test_single_tag_and_content():
 
 
 def test_single_tag_content_and_attributes():
-    assert myxml("foo", "bar", a=1, b=2, c=3) == "<foo a=1 b=2 c=3>bar</foo>"
+    assert myxml("foo", "bar", a=1, b=2, c=3) == '<foo a="1" b="2" c="3">bar</foo>'
+
+
+def test_nested():
+    assert myxml("a", myxml("b", myxml("c", "text"))) == "<a><b><c>text</c></b></a>"
+
+
+def test_attributes():
+    assert myxml("tagname", a=1, b=2, c=3) == '<tagname a="1" b="2" c="3"></tagname>'
+
+
+def test_attributes_and_text():
+    assert (
+        myxml("tagname", "text", a=1, b=2, c=3)
+        == '<tagname a="1" b="2" c="3">text</tagname>'
+    )
 
 
 ################################################################################
@@ -33,3 +50,116 @@ def test_two_nums():
 
 def test_three_nums():
     assert factorial(1, 2, 3) == 6
+
+
+################################################################################
+# Prefix Notation Calculator
+
+
+@pytest.mark.parametrize(
+    "inputs, expected",
+    [
+        ("+12", 3),
+        ("+123", 6),
+        ("+1234", 10),
+    ],
+)
+def test_addition(inputs, expected):
+    assert calc(inputs) == expected
+
+
+@pytest.mark.parametrize(
+    "inputs, expected",
+    [
+        ("-12", -1),
+        ("-123", -4),
+        ("-1234", -8),
+    ],
+)
+def test_subtraction(inputs, expected):
+    assert calc(inputs) == expected
+
+
+@pytest.mark.parametrize(
+    "inputs, expected",
+    [
+        ("*12", 2),
+        ("*123", 6),
+        ("*1234", 24),
+    ],
+)
+def test_multiplication(inputs, expected):
+    assert calc(inputs) == expected
+
+
+@pytest.mark.parametrize(
+    "inputs, expected",
+    [
+        ("/12", 0.5),
+        ("/124", 0.125),
+        ("/1248", 0.015625),
+    ],
+)
+def test_division(inputs, expected):
+    assert calc(inputs) == expected
+
+
+@pytest.mark.parametrize(
+    "inputs, expected",
+    [
+        ("%12", 1),
+        ("%123", 1),
+        ("%1234", 1),
+    ],
+)
+def test_modulo(inputs, expected):
+    assert calc(inputs) == expected
+
+
+@pytest.mark.parametrize(
+    "inputs, expected",
+    [
+        ("**12", 1),
+        ("**123", 1),
+        ("**1234", 1),
+    ],
+)
+def test_exponential(inputs, expected):
+    assert calc(inputs) == expected
+
+
+################################################################################
+# Password Generator
+
+
+@pytest.mark.parametrize(
+    "pool, size, pw",
+    [
+        ("abcdef", 8, "ddaceddc"),
+        ("!@#$%", 8, "$$!#%$$#"),
+        (string.ascii_lowercase, 20, "mynbiqpmzjplsgqejeyd"),
+    ],
+)
+def test_simple_generator(pool, size, pw):
+    random.seed(0)
+    create_password = create_password_generator(pool)
+    output = create_password(size)
+    assert len(output) == size
+    assert output == pw
+
+
+@pytest.mark.parametrize(
+    "min_uppercase, min_lowercase, min_punctuation, min_digits, pw, passes_test",
+    [
+        (1, 4, 1, 1, "Ufeda!123", True),
+        (2, 4, 4, 3, "thisisnotgoingtopass", False),
+    ],
+)
+def test_simple_checker(
+    min_uppercase, min_lowercase, min_punctuation, min_digits, pw, passes_test
+):
+    password_checker = create_password_checker(
+        min_uppercase, min_lowercase, min_punctuation, min_digits
+    )
+    result = password_checker(pw)
+    assert result == passes_test
