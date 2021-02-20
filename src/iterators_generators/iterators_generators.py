84c5a085ec22c15cda0af18c74e0e6c99f119beb
tnakatani <4361007+tnakatani@users.noreply.github.com>
2021-02-19 20:03:40 -0500

    finish iterators generators code and test

--- src/iterators_generators/iterators_generators.py
+++ src/iterators_generators/iterators_generators.py
@@ -14,12 +14,14 @@ BTE 1: Rewrite MyEnumerate such that it uses a helper class (MyEnumerateIterator
 BTE 2: Add a second, optional argument--an integer, representing the first index that should be
        used.  This is handy for implementing one-indexed arrays.
 """
+import collections
 import os
-from typing import Iterable, List
+import time
+from typing import Generator, Iterable, TextIO
 
 
 class MyEnumerate:
-    def __init__(self, data, start: int = 0):
+    def __init__(self, data):
         self.data = data
         self.index = 0
 
@@ -45,7 +47,7 @@ class MyEnumerateIterator:
 def my_generator(data, start: int = 0):
     index = start
     for i in data:
-        yield (index, data[index])
+        yield index, data[index]
         index += 1
 
 
@@ -102,7 +104,7 @@ be returned.
 """
 
 
-class Circle_2:
+class Circle2:
     def __init__(self, data, end: int = 0):
         self.data = data
         self.index = 0
@@ -124,7 +126,7 @@ class Circle_2:
         return type(self)(self.data, self.end)
 
 
-class CircleInherit(Circle_2):
+class CircleInherit(Circle2):
     def __init__(self, data, end: int = 0):
         super().__init__(data, end)
         self.returns = "data"
@@ -133,13 +135,6 @@ class CircleInherit(Circle_2):
 """
 BTE 2: Implement Circle as a generator function, rather than as a class.
 """
-
-
-def my_generator(data, start: int = 0):
-    index = start
-    for i in data:
-        yield (index, data[index])
-        index += 1
 
 
 def circle(data, end: int = 0):
@@ -165,8 +160,8 @@ integer arguments.
 class MyRange:
     def __init__(self, start: int, stop: int = None, step: int = 1):
         if stop is None:
-            self.start = 0
             self.stop = start
+            self.start = 0
         else:
             self.start = start
             self.stop = stop
@@ -183,6 +178,7 @@ class MyRange:
     def __next__(self):
         if self.start >= self.stop:
             raise StopIteration
+
         value = self.start
         self.start += self.step
         return value
@@ -205,7 +201,7 @@ and the current line.
 """
 
 
-def all_lines(path: str) -> Iterable:
+def all_lines(path: str) -> Generator:
     index = 0
     for file in os.listdir(path):
         fp = os.path.join(path, file)
@@ -239,14 +235,14 @@ those lines containing the string (i.e., for which you can say s in line) should
 """
 
 
-def open_file_safely(filename: str) -> List[Iterable]:
+def open_file_safely(filename: str) -> TextIO:
     try:
         return open(filename)
     except OSError:
         pass
 
 
-def parallel_lines(path: str, filter_str: str = None) -> Iterable:
+def parallel_lines(path: str, filter_str: str = None) -> Generator:
     """Returns the first line from each file, and then the second line from each file, until all
     lines from all files are returned. When you finish printing lines from shorter files,
     ignore those files while continuing to display lines from the longer files"""
@@ -288,3 +284,217 @@ def parallel_lines(path: str, filter_str: str = None) -> Iterable:
     #             pass
     #         finally:
     #             file_index += i
+
+
+####################################################################################################
+# e49: Elapsed Since
+
+"""
+Write a generator function whose argument must be iterable. With each iteration, the generator 
+will return a two-element tuple. The first element in the tuple will be an integer indicating how 
+many seconds have passed since the previous iteration. The tuple’s second element will be the 
+next item from the passed argument.You can use time.perf_counter, which returns the number of 
+seconds since the program was started. 
+
+BTE 1: Add an argument for the minimum amount of time that must elapse between iterations. If the 
+next element is requested via the iterator protocol (i.e., next), and the time elapsed since the 
+previous iteration is greater than the user-defined minimum, then the value is returned. If not, 
+then the generator uses time.sleep to wait until the appropriate amount of time has elapsed.
+"""
+
+
+def elapsed_since(data: Iterable, min_wait: int = 0):
+    """A generator that takes an iterable as input. With each iteration, it yields a tuple
+    containing the time since the previous iteration and the data.
+
+    If the next element is requested via the iterator protocol (i.e., next), and the time elapsed
+    since the previous iteration is greater than the user-defined minimum, then the value is
+    returned.
+    """
+    last_time = None  # Must be None for or logic to work below since 0 is True
+    for i in data:
+        current_time = time.perf_counter()
+        delta = current_time - (last_time or current_time)
+        if delta < min_wait:
+            delta = min_wait - delta
+            time.sleep(delta)
+        last_time = time.perf_counter()
+        yield delta, i
+
+    # Attempt 1: Works
+    # last_time = None  # Must be None for or logic to work below since 0 is True
+    # for i in data:
+    #     current_time = time.perf_counter()
+    #     delta = current_time - (last_time or current_time)
+    #     last_time = time.perf_counter()
+    #     if delta < min_elapsed:
+    #         new_elapsed = min_elapsed - delta
+    #         time.sleep(new_elapsed)
+    #         yield new_elapsed, i
+    #     else:
+    #         yield delta, i
+
+
+"""
+BTE 2: Write a generator function, file_usage_timing, that takes a single directory name as an 
+argument. With each iteration, we get a tuple containing not just the current filename, but also 
+the three reports that we can get about a file’s most recent usage: its access time (atime), 
+modification time (mtime), and creation time (ctime). Hint: all are available via the os.stat 
+function.
+"""
+
+
+def file_usage_timing(path: str) -> Generator:
+    for file in os.listdir(path):
+        info = os.stat(os.path.join(path, file))
+        yield file, info.st_atime, info.st_mtime, info.st_ctime
+
+
+"""
+BTE 3: Write a generator function that takes two elements: an iterable and a function. With each 
+iteration, the function is invoked on the current element. If the result is True, then the 
+element is returned as is. Otherwise, the next element is tested, until the function returns 
+True. Alternative: implement this as a regular function that returns a generator expression.
+"""
+
+
+def yield_filter(data: Iterable):
+    for i in data:
+        if bytes(i, "utf-8"):
+            yield i
+
+
+####################################################################################################
+# e50: MyChain
+
+"""
+Implement a generator function called mychain that emulates itertool's chain method. It takes any 
+number of arguments, each of which is an iterable.  With each iteration, it should return the next 
+element from the current iterable, or the first element from the subsequent iterable--unless 
+you’re at the end, in which case it should exit.
+"""
+
+
+def mychain(*args):
+    for arg in args:
+        for item in arg:
+            yield item
+
+
+"""
+BTE 1: The built-in zip function returns an iterator that, given iterable arguments, 
+returns tuples taken from those arguments’ elements. The first iteration will return a tuple from 
+the arguments’ index 0, the second iteration will return a tuple from the arguments’ index 1, 
+and so on, stopping when the shortest of the arguments ends. Thus zip('abc', [10, 20, 
+30]) returns the iterator equivalent of [('a', 10), ('b', 20), ('c', 30)]. Write a generator 
+
+function that reimplements zip in this way.
+"""
+
+
+class MyZip:
+    @staticmethod
+    def is_mapping(obj: Iterable) -> bool:
+        # https://docs.python.org/3/glossary.html#term-mapping
+        return isinstance(obj, collections.abc.Mapping)
+
+    def zip(self, *args) -> tuple:
+        """Implementation of zip().  For dicts, returns the dictionary keys"""
+        # get the shortest length among iterables
+        min_arg_length = len(min(args, key=len))
+        for i in range(min_arg_length):
+            output = []
+            for arg in args:
+                # check if object is a dictionary
+                if self.is_mapping(arg):
+                    keys = list(arg.keys())
+                    output.append(keys[i])
+                    continue
+                output.append(arg[i])
+            yield tuple(o for o in output)
+
+    # Native python version for comparison
+    @staticmethod
+    def python_zip(*iterables):
+        """Native Python's implementation of zip"""
+        # zip('ABCD', 'xy') --> Ax By
+        sentinel = object()
+        iterators = [iter(it) for it in iterables]
+        while iterators:
+            result = []
+            for it in iterators:
+                elem = next(it, sentinel)
+                if elem is sentinel:
+                    return
+                result.append(elem)
+            yield tuple(result)
+
+    # Attempt 1: Doesn't work
+    # while min_length:
+    #     output = []
+    #     for arg in args:
+    #         print(arg)
+    #         output.append(arg[counter])
+    #     min_length -= 1
+    #     yield output
+
+
+"""
+BTE 2: Reimplement the all_lines function from exercise 49 using mychain.
+"""
+
+
+def all_lines_mychain(path: str) -> Generator:
+    files = (
+        open(os.path.join(path, file))
+        for file in os.listdir(path)
+        if os.path.isfile(os.path.join(path, file))
+    )
+    return mychain(*files)
+
+    # Attempt 1: Works
+    # files = []
+    # for file in os.listdir(path):
+    #     try:
+    #         files.append(open(os.path.join(path, file)))
+    #     except OSError:
+    #         pass
+    # return mychain(*files)
+
+    # Book solution
+    # return mychain(*(open(os.path.join(path, filename))
+    #                  for filename in os.listdir(path)
+    #                  if os.path.isfile(os.path.join(path, filename))))
+
+
+"""
+BTE 3: In the “Beyond the exercise” section for exercise 48, you implemented a MyRange class, 
+which mimics the built-in range class. Now do the same thing, but using a generator expression.
+"""
+
+def myrange_generator(start: int, stop: int = None, step: int = 1) -> Generator:
+    if stop is None:
+        stop = start
+        start = 0
+    else:
+        start = start
+        stop = stop
+
+    while start < stop:
+        value = start
+        start += step
+        yield value
+
+# Book solution
+# def myrange(first, second=None, step=1):
+#     if second is None:
+#         current = 0
+#         stop = first
+#
+#     else:
+#         current = first
+#         stop = second
+#
+#     while current < stop:
+#         yield current
+#         current += step
--- test/iterators_generators/test_iterators_generators.py
+++ test/iterators_generators/test_iterators_generators.py
@@ -1,143 +1,142 @@
-import os
+import time
 
-import mock
 import pytest
 
 from src.iterators_generators.iterators_generators import (
     Circle,
     CircleInherit,
-    Circle_2,
     MyEnumerate,
     MyRange,
+    MyZip,
     all_lines,
-    circle,
-    my_generator, parallel_lines,
+    all_lines_mychain, circle,
+    elapsed_since,
+    file_usage_timing,
+    my_generator,
+    mychain,
+    myrange_generator, parallel_lines,
+    yield_filter,
 )
 
 ####################################################################################################
 # e47: MyEnumerate
 
 
-@pytest.mark.parametrize(
-    "inputs, expected",
-    [
-        (
-            ["Chocolate", "Vanilla", "Strawberry"],
-            [(0, "Chocolate"), (1, "Vanilla"), (2, "Strawberry")],
-        ),
-        (
-            [1, 2, 3],
-            [(0, 1), (1, 2), (2, 3)],
-        ),
-    ],
-)
-def test_myenumerate(inputs, expected):
-    e = MyEnumerate(inputs)
-    e_indexed = [(i, v) for i, v in e]
-    assert e_indexed == expected
+class TestMyEnumerate:
+    @pytest.mark.parametrize(
+        "inputs, expected",
+        [
+            (
+                ["Chocolate", "Vanilla", "Strawberry"],
+                [(0, "Chocolate"), (1, "Vanilla"), (2, "Strawberry")],
+            ),
+            (
+                [1, 2, 3],
+                [(0, 1), (1, 2), (2, 3)],
+            ),
+        ],
+    )
+    def test_myenumerate(self, inputs, expected):
+        e = MyEnumerate(inputs)
+        e_indexed = [(i, v) for i, v in e]
+        assert e_indexed == expected
 
+    def test_myenumerate_enumerating_twice(self):
+        e = MyEnumerate("abc")
+        e_indexed = [(i, v) for i, v in e]
+        f_indexed = [(i, v) for i, v in e]
+        assert e_indexed == [(0, "a"), (1, "b"), (2, "c")]
+        assert f_indexed == [(0, "a"), (1, "b"), (2, "c")]
 
-def test_myenumerate_enumerating_twice():
-    e = MyEnumerate("abc")
-    e_indexed = [(i, v) for i, v in e]
-    f_indexed = [(i, v) for i, v in e]
-    assert e_indexed == [(0, "a"), (1, "b"), (2, "c")]
-    assert f_indexed == [(0, "a"), (1, "b"), (2, "c")]
+    def test_myenumerate_is_iterable(self):
+        e = MyEnumerate("abc")
+        assert hasattr(e, "__iter__")
 
-
-def test_myenumerate_is_iterable():
-    e = MyEnumerate("abc")
-    assert hasattr(e, "__iter__")
-
-
-@pytest.mark.parametrize(
-    "inputs, expected",
-    [
-        (
-            ["Chocolate", "Vanilla", "Strawberry"],
-            [(0, "Chocolate"), (1, "Vanilla"), (2, "Strawberry")],
-        ),
-        (
-            [1, 2, 3],
-            [(0, 1), (1, 2), (2, 3)],
-        ),
-    ],
-)
-def test_mygenerator(inputs, expected):
-    g = my_generator(inputs)
-    assert [(i, v) for i, v in g] == expected
+    @pytest.mark.parametrize(
+        "inputs, expected",
+        [
+            (
+                ["Chocolate", "Vanilla", "Strawberry"],
+                [(0, "Chocolate"), (1, "Vanilla"), (2, "Strawberry")],
+            ),
+            (
+                [1, 2, 3],
+                [(0, 1), (1, 2), (2, 3)],
+            ),
+        ],
+    )
+    def test_mygenerator(self, inputs, expected):
+        g = my_generator(inputs)
+        assert [(i, v) for i, v in g] == expected
 
 
 ####################################################################################################
 # e48: Circle
 
 
-@pytest.mark.parametrize(
-    "inputs, expected",
-    [
-        (
-            ["a", "b", "c"],
-            ["a", "b", "c", "a", "b", "c"],
-        ),
-        (
-            [1, 2],
-            [1, 2, 1, 2, 1, 2],
-        ),
-    ],
-)
-def test_circle(inputs, expected):
-    c = Circle(inputs, 6)
-    assert [i for i in c] == expected
+class TestCircle:
+    @pytest.mark.parametrize(
+        "inputs, expected",
+        [
+            (
+                ["a", "b", "c"],
+                ["a", "b", "c", "a", "b", "c"],
+            ),
+            (
+                [1, 2],
+                [1, 2, 1, 2, 1, 2],
+            ),
+        ],
+    )
+    def test_circle(self, inputs, expected):
+        c = Circle(inputs, 6)
+        assert [i for i in c] == expected
 
+    def test_circle_inherit(self):
+        c = CircleInherit("abc", 6)
+        assert [i for i in c] == ["a", "b", "c", "a", "b", "c"]
 
-def test_circle_inherit():
-    c = CircleInherit("abc", 6)
-    assert [i for i in c] == ["a", "b", "c", "a", "b", "c"]
+    @pytest.mark.parametrize(
+        "inputs, expected",
+        [
+            (
+                ["a", "b", "c"],
+                ["a", "b", "c", "a", "b", "c"],
+            ),
+            (
+                [1, 2],
+                [1, 2, 1, 2, 1, 2],
+            ),
+        ],
+    )
+    def test_circle_generator(self, inputs, expected):
+        c = circle(inputs, 6)
+        assert [i for i in c] == expected
 
+    @pytest.mark.parametrize(
+        "inputs, expected",
+        [
+            (
+                [0, 10, 2],
+                [0, 2, 4, 6, 8],
+            ),
+            (
+                [0, 20, 10],
+                [0, 10],
+            ),
+        ],
+    )
+    def test_myrange(self, inputs, expected):
+        r = MyRange(inputs[0], inputs[1], inputs[2])
+        assert [i for i in r] == expected
 
-@pytest.mark.parametrize(
-    "inputs, expected",
-    [
-        (
-            ["a", "b", "c"],
-            ["a", "b", "c", "a", "b", "c"],
-        ),
-        (
-            [1, 2],
-            [1, 2, 1, 2, 1, 2],
-        ),
-    ],
-)
-def test_circle_generator(inputs, expected):
-    c = circle(inputs, 6)
-    assert [i for i in c] == expected
-
-
-@pytest.mark.parametrize(
-    "inputs, expected",
-    [
-        (
-            [0, 10, 2],
-            [0, 2, 4, 6, 8],
-        ),
-        (
-            [0, 20, 10],
-            [0, 10],
-        ),
-    ],
-)
-def test_myrange(inputs, expected):
-    r = MyRange(inputs[0], inputs[1], inputs[2])
-    assert [i for i in r] == expected
-
-
-def test_myrange_with_defaults():
-    r = MyRange(0, 5)
-    s = MyRange(10)
-    t = MyRange(0)
-    assert [i for i in r] == [0, 1, 2, 3, 4]
-    assert [i for i in s] == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    assert [i for i in t] == []
+    def test_myrange_with_defaults(self):
+        r = MyRange(0, 5)
+        s = MyRange(10)
+        t = MyRange(0)
+        assert [i for i in r] == [0, 1, 2, 3, 4]
+        assert [i for i in s] == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        assert [i for i in t] == []
 
 
 ####################################################################################################
@@ -150,7 +149,6 @@ def test_dir(tmp_path):
     g = tmp_path / "f2.txt"
     h = tmp_path / "f3.txt"
     i = tmp_path / "f4.txt"
-
     f.write_text("\n".join(["foo", "bar", "baz"]))
     g.write_text("\n".join(["one", "two", "three"]))
     h.write_text(
@@ -163,26 +161,119 @@ but the biggest word will probably be encyclopedia"""
     return tmp_path
 
 
-def test_all_lines(test_dir):
-    assert len([i for i in all_lines(test_dir)]) == 10
-
-
 @pytest.fixture
 def test_empty_dir(tmp_path):
     return tmp_path
 
 
-def test_all_lines_empty_dir(test_empty_dir):
-    assert len([i for i in all_lines(test_empty_dir)]) == 0
+class TestAlloftheFiles:
+    def test_all_lines(self, test_dir):
+        assert len([i for i in all_lines(test_dir)]) == 10
+
+    def test_all_lines_empty_dir(self, test_empty_dir):
+        assert len([i for i in all_lines(test_empty_dir)]) == 0
+
+    def test_parallel_lines(self, test_dir):
+        result = [i for i in parallel_lines(test_dir) if i]
+        assert len(result) == 10
+
+    def test_parallel_lines_with_filter(self, test_dir):
+        result = [i for i in parallel_lines(test_dir, "foo") if i]
+        assert len(result) == 1
 
 
-def test_parallel_lines(test_dir):
-    result = [i for i in parallel_lines(test_dir) if i]
-    print(result)
-    assert len(result) == 10
+####################################################################################################
+# e49: Elapsed Since
+# TODO: Learn how to mock time.perf_counter
+class TestElapsedSince:
+    def test_elapsed_since(self):
+        for index, t in enumerate(elapsed_since("abc")):
+            assert isinstance(t, tuple)
+            assert isinstance(t[0], float)
+            assert isinstance(t[1], str)
+
+            if index == 0:
+                assert t[0] == 0
+            else:
+                assert round(t[0], 1) == 0.1
+
+            time.sleep(0.1)
+
+    def test_elapsed_with_min_elapsed(self):
+        time_elapsed = 0.5
+        for index, t in enumerate(elapsed_since("abc", min_wait=time_elapsed)):
+            assert isinstance(t, tuple)
+            assert isinstance(t[0], float)
+            assert isinstance(t[1], str)
+
+            if index == 0:
+                assert t[0] == time_elapsed
+            else:
+                assert round(t[0], 1) == time_elapsed
+
+    # BTE 2
+    # TODO: Mock os.stat attributes
+    # Uses test_dir fixture
+    def test_file_usage_timing(self, test_dir):
+        for i in file_usage_timing(test_dir):
+            assert len(i) == 4
+
+    # BTE 3
+    def test_yield_filter(self):
+        data = ["foo", "", "bar", "", "baz"]
+        assert len([i for i in yield_filter(data)]) == 3
 
 
-def test_parallel_lines_with_filter(test_dir):
-    result = [i for i in parallel_lines(test_dir, 'foo') if i]
-    print(result)
-    assert len(result) == 1
+class TestMyChain:
+    data = ("abc", [1, 2, 3], {"foo": 1, "bar": 2})
+
+    def test_mychain(self):
+        assert [i for i in mychain(*self.data)] == [
+            "a",
+            "b",
+            "c",
+            1,
+            2,
+            3,
+            "foo",
+            "bar",
+        ]
+
+    def test_myzip(self):
+        z = MyZip()
+        for i, j, k in zip(
+            z.zip(*self.data), zip(*self.data), z.python_zip(*self.data)
+        ):
+            assert i == j == k
+
+
+class TestAllLinesMyChain:
+    def test_all_lines_mychain(self, test_dir):
+        assert len([i for i in all_lines_mychain(test_dir)]) == 10
+
+
+class TestMyRangeGenerator:
+    @pytest.mark.parametrize(
+        "inputs, expected",
+        [
+            (
+                    [0, 10, 2],
+                    [0, 2, 4, 6, 8],
+            ),
+            (
+                    [0, 20, 10],
+                    [0, 10],
+            ),
+        ],
+    )
+    def test_myrange_generator(self, inputs, expected):
+        r = myrange_generator(inputs[0], inputs[1], inputs[2])
+        assert [i for i in r] == expected
+
+    def test_myrange_generator_with_defaults(self):
+        r = myrange_generator(0, 5)
+        s = myrange_generator(10)
+        t = myrange_generator(0)
+        assert [i for i in r] == [0, 1, 2, 3, 4]
+        assert [i for i in s] == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        assert [i for i in t] == []
